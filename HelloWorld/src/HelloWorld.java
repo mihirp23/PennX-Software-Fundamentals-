a5f362f7c8c7a80466d2cfdfcdea3a0a3d97d7eb
Mihir <patelm48@student.wpunj.edu>
2017-05-21 11:28:28 -0400

    add HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+// Name : Mihir Patel
+// Date : May 21, 2017
+// Desc : Display a greeting to the world.
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
