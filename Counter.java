9acb702b969b78c4f5979f99fc08a3f7bc4974d3
Mihir <patelm48@student.wpunj.edu>
2017-05-27 12:30:22 -0400

    Add Counter and CounterTest

---
+++ Counter.java
@@ -0,0 +1,21 @@
+
+public class Counter {
+    int val;
+    
+    // constructor
+	public Counter() {
+    	val = 0;
+    }
+	
+	// increase the value by one and return the new value
+	public int increment() {
+		val++;
+		return val;
+	}
+	
+	// decrease the value by one and return the value
+	public int decrement() {
+		val--;
+		return val;
+	}
+}
---
+++ CounterTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CounterTest {
+	
+	Counter counter1;
+
+	@Before
+	public void setUp() throws Exception {
+		counter1 = new Counter();
+	}
+
+	@Test
+	public void testIncrement() {
+		assertTrue(counter1.increment() == 1);
+		assertTrue(counter1.increment() == 2);
+	}
+
+	@Test
+	public void testDecrement() {
+		assertTrue(counter1.decrement() == -1);
+		assertTrue(counter1.decrement() == -2);
+	}
+
+}
