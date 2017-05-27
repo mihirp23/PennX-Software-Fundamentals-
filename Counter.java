5b5f1c5fa9d5d5197f81901b61dc85026c19e011
Mihir <patelm48@student.wpunj.edu>
2017-05-27 12:32:08 -0400

    Indent Counter.java

--- Counter.java
+++ Counter.java
@@ -7,15 +7,15 @@ public class Counter {
     	val = 0;
     }
 	
-	// increase the value by one and return the new value
-	public int increment() {
-		val++;
-		return val;
-	}
+   // increase the value by one and return the new value
+   public int increment() {
+	val++;
+	return val;
+   }
 	
-	// decrease the value by one and return the value
-	public int decrement() {
-		val--;
-		return val;
-	}
+   // decrease the value by one and return the value
+   public int decrement() {
+	val--;
+	return val;
+   }
 }
