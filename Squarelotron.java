0668e032915b01be18cbb80cb340aca400860ca0
Mihir <patelm48@student.wpunj.edu>
2017-05-31 12:39:38 -0400

    Add JUnit test cases

--- Squarelotron.java
+++ Squarelotron.java
@@ -1,4 +1,6 @@
-import java.util.*;
+// Name : Mihir Patel
+// Date : May 31, 2017
+// Desc : Manipulate squarelotron.
 
 public class Squarelotron {
 	int[][] squarelotron = new int[3][3];
@@ -17,7 +19,8 @@ public class Squarelotron {
 		this.size = n;
 	}
 
-	
+	// This method performs the Upside-Down Flip of the squarelotron,
+	// and returns the new squarelotron.
 	public Squarelotron upsideDownFlip(int ring) {
 
 		Squarelotron t = new Squarelotron(this.size);
@@ -42,7 +45,8 @@ public class Squarelotron {
 
 	}
 
-	
+	// This method performs the Main Diagonal Flip of the squarelotron,
+	// and returns the new squarelotron. 
 	public Squarelotron mainDiagonalFlip(int ring) {
 		Squarelotron t = new Squarelotron(this.size);
 		ring--;
@@ -72,28 +76,67 @@ public class Squarelotron {
 	}
 
 	
+	// The argument numberOfTurns indicates the number of times the entire squarelotron 
+	// should be rotated 90 degrees clockwise. Any integer, including zero and negative integers, 
+	// is allowable as the argument. A value of -1 indicates a 90 degrees counterclockwise
+	// rotation. This method modifies the internal representation of the squarelotron
 	void rotateRight(int numberOfTurns) {
 
+		if (numberOfTurns > 0) {
+			numberOfTurns = numberOfTurns % 4;
+		} else if (numberOfTurns < 0) {
+			numberOfTurns = (4 + numberOfTurns) % numberOfTurns;
+		}
+
+		if (numberOfTurns == 0) {
+			return; // no need to rotate
+		}
+
+		Squarelotron t = new Squarelotron(this.size);
+
+		for (int i = 0; i < this.size; i++) {
+			for (int j = 0; j < this.size; j++) {
+
+				int pos_i, pos_j;
+				if (numberOfTurns == 1) {
+					pos_i = j;
+					pos_j = this.size - 1 - i;
+				} else if (numberOfTurns == 2) {
+					pos_i = this.size - 1 - i;
+					pos_j = this.size - 1 - j;
+				} else {
+					pos_i = this.size - 1 - j;
+					pos_j = i;
+				}
+				t.squarelotron[pos_i][pos_j] = this.squarelotron[i][j];
+			}
+
+		}
+
+		this.squarelotron = t.squarelotron;
+
 	}
-    @Override
-    public boolean equals(Object obj) {
-    	if (obj == null) {
-    		return false;
-    	}
-    	final Squarelotron t = (Squarelotron) obj;
-        if (this.size != t.size) {
-        	return false;
-        }
-        for (int i = 0; i < this.size; i++) {
-        	for (int j = 0; j < this.size; j++) {
-        		if (this.squarelotron[i][j] != t.squarelotron[i][j]) {
-        			return false;
-        		}
-        	}
-        }
-    	return true;
-    }
-    
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		final Squarelotron t = (Squarelotron) obj;
+		if (this.size != t.size) {
+			return false;
+		}
+		for (int i = 0; i < this.size; i++) {
+			for (int j = 0; j < this.size; j++) {
+				if (this.squarelotron[i][j] != t.squarelotron[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// display the squarelotron
 	public void display() {
 		for (int i = 0; i < this.size; i++) {
 			for (int j = 0; j < this.size; j++) {
@@ -105,12 +148,12 @@ public class Squarelotron {
 	}
 
 	public static void main(String[] args) {
-		Squarelotron s = new Squarelotron(4);
-		s.display();
-		Squarelotron t = new Squarelotron(4);
-		t = s.mainDiagonalFlip(1);
-		t.display();
 		
+		Squarelotron t = new Squarelotron(3);
+		
+		t.display();
+		t.rotateRight(1);
+		t.display();
 	}
 
 }
--- SquarelotronTest.java
+++ SquarelotronTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SquarelotronTest {
+	
+	Squarelotron mySq = new Squarelotron(3);
 
 	@Before
 	public void setUp() throws Exception {
@@ -11,22 +13,33 @@ public class SquarelotronTest {
 
 	@Test
 	public void testSquarelotron() {
-		fail("Not yet implemented");
+		assert(mySq.size == 3);
 	}
 
 	@Test
 	public void testUpsideDownFlip() {
-		fail("Not yet implemented");
+		int t[][] = {{7,8,9}, {4,5,6}, {1,2,3}};
+		Squarelotron s = new Squarelotron(mySq.size);
+		s.squarelotron = t;
+		assertEquals(mySq.upsideDownFlip(1), s);
+		
 	}
 
 	@Test
 	public void testMainDiagonalFlip() {
-		fail("Not yet implemented");
+		int t[][] = {{1,4,7}, {2,5,8}, {3,6,9}};
+		Squarelotron s = new Squarelotron(mySq.size);
+		s.squarelotron = t;
+		assertEquals(mySq.mainDiagonalFlip(1), s);
 	}
 
 	@Test
 	public void testRotateRight() {
-		fail("Not yet implemented");
+		int t[][] = {{7,4,1}, {8,5,2}, {9,6,3}};
+		Squarelotron s = new Squarelotron(mySq.size);
+		s.squarelotron = t;
+		mySq.rotateRight(1);
+		assertEquals(mySq, s);
 	}
 
 }
